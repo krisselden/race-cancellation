8560ea86dd965f75f68d99aa6ae157e65a17a68e
Kris Selden <kris.selden@gmail.com>
2018-12-19 17:51:08 -0800

    Try to make more clear, make it about racing the winning chain when chains are joined by Promise.all or Promise.race

--- src/run.ts
+++ src/run.ts
@@ -8,27 +8,26 @@ const throwShortCircuited = () => {
 };
 
 /**
- * Run a task with a race of the task exit so that any short-circuited child tasks
- * due to `Promise.all` or `Promise.race` can be raced against their parents exit.
+ * Run a task with a race of the task winning promise chain so that any short-circuited child
+ * chains due to `Promise.all` or `Promise.race` can be raced against the winner.
  *
  * If race function is never called a promise for the cancellation will
  * never be created so that exit will not generate any unhandled rejections.
  *
- * If the parent task has exited already, the subtask passed to race will not
+ * If the parent task has resolved already, the subtask passed to race will not
  * be started.
  *
  * @param task a cancellable task
- * @param raceCancel the outside race cancellation
+ * @param raceCancellation optional outer race cancellation
  */
 export default async function run<Result>(
   task: CancellableTask<Result>,
-  raceCancel?: Race
+  raceCancellation?: Race
 ): Promise<Result> {
-  const [raceExit, onExit] = cancellableRace(throwShortCircuited);
+  const [raceWinner, cancelLosers] = cancellableRace(throwShortCircuited);
   try {
-    return await task(combineRace(raceCancel, raceExit));
+    return await task(combineRace(raceCancellation, raceWinner));
   } finally {
-    // ensure we cancel losers in the above promise chain
-    onExit();
+    cancelLosers();
   }
 }
