0285a8d2585f1e3abe7cb95f0512ad7acd7b453b
Kris Selden <kris.selden@gmail.com>
2018-12-19 19:08:36 -0800

    Ctrl-C example

--- examples/ctrl-c.js
+++ examples/ctrl-c.js
@@ -13,28 +13,42 @@ const throwInterrupt = () => {
 
 const [raceInterrupt, onInterrupt] = cancellableRace(throwInterrupt);
 
-run(main, raceInterrupt).catch(e => {
-  console.error(e);
-  process.exitCode = 1;
-});
-
 process.on("SIGINT", onInterrupt);
+
+run(main, raceInterrupt);
 
 /**
  * main cancellable async task
  * @param {Race} raceCancellation
  */
 async function main(raceCancellation) {
+  console.log("main started");
   try {
-    console.log("main start");
+    const result = await doSomeAsyncTask((step, total) => {
+      console.log(`main progress: ${step} of ${total}`);
+    }, raceCancellation);
+    console.log(`main done: ${result}`);
+  } catch (e) {
+    console.error(e.stack);
+    process.exitCode = 1;
+  } finally {
+    console.log("main finalized");
+  }
+}
+
+/**
+ * main cancellable async work task
+ * @param {(i: number, t: number) => void} progress
+ * @param {Race} raceCancellation
+ */
+async function doSomeAsyncTask(progress, raceCancellation) {
+  try {
     for (let i = 0; i < 8; i++) {
       await sleep(1000, raceCancellation);
-      console.log(i + 1);
+      progress(i + 1, 8);
     }
-    console.log("main done");
   } catch (e) {
-    throw new Error(`main error: ${e.stack}`);
-  } finally {
-    console.log("main finally");
+    throw new Error(`doSomeAsyncThing failed: ${e.message}`);
   }
+  return "some result";
 }
