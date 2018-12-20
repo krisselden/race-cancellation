5ce01ad5f850a94d3d006d3d41f845682e2c1f6e
Kris Selden <kris.selden@gmail.com>
2018-12-19 20:16:09 -0800

    use disposablePromise instead of sleep to make pattern more clear

--- examples/ctrl-c.js
+++ examples/ctrl-c.js
@@ -1,22 +1,26 @@
 const {
   cancellableRace,
   cancellationError,
+  disposablePromise,
   run,
-  sleep,
 } = require("race-cancellation");
 
 /** @typedef {import("race-cancellation").Race} Race */
 
+// cancellableRace returns a [raceFunc, cancelFunc]
 const [raceInterrupt, onInterrupt] = cancellableRace(() => {
   throw cancellationError("SIGINT");
 });
 
 process.on("SIGINT", onInterrupt);
 
+// `run` ensures raceCancellation passed to the async function
+// will cancel losing promise chains due to short-circuiting
+// in `Promise.all` or `Promise.race`.
 run(main, raceInterrupt);
 
 /**
- * cancellable async main with graceful termination on cancel.
+ * Cancellable async main with graceful termination on cancel.
  * @param {Race} raceCancellation
  */
 async function main(raceCancellation) {
@@ -28,6 +32,8 @@ async function main(raceCancellation) {
     console.log(`main done: ${result}`);
   } catch (e) {
     console.error(e.stack);
+    // since cancellation runs finalizers for losing promise chain
+    // we don't need to force node to exit.
     process.exitCode = 1;
   } finally {
     console.log("main finalized");
@@ -42,11 +48,25 @@ async function main(raceCancellation) {
 async function doSomeAsyncTask(progress, raceCancellation) {
   try {
     for (let i = 0; i < 8; i++) {
-      await sleep(1000, raceCancellation);
+      await doRealAsyncStep(raceCancellation);
       progress(i + 1, 8);
     }
   } catch (e) {
     throw new Error(`doSomeAsyncThing failed: ${e.message}`);
   }
   return "some result";
+}
+
+/**
+ * @param {Race} raceCancellation
+ */
+function doRealAsyncStep(raceCancellation) {
+  // `disposablePromise` is helper real async to
+  // ensure cleanup on cancellation
+  return disposablePromise(resolve => {
+    const id = setTimeout(resolve, 1000);
+    return () => {
+      clearTimeout(id);
+    };
+  }, raceCancellation);
 }
