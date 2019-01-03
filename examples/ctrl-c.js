a40b3849fab7d74e5df968d236fb43acbd2a269a
Kris Selden <kris.selden@gmail.com>
2019-01-03 11:19:52 -0800

    more example cleanup

--- examples/ctrl-c.js
+++ examples/ctrl-c.js
@@ -4,8 +4,6 @@ const {
   disposablePromise,
 } = require("race-cancellation");
 
-/** @typedef {import("race-cancellation").Race} Race */
-
 const [raceCancellation, cancel] = cancellableRace(() => {
   throw cancellationError("SIGINT");
 });
@@ -16,49 +14,45 @@ main();
 
 /**
  * Cancellable async main with graceful termination on cancel.
- * @param {Race} raceCancellation
  */
 async function main() {
   console.log("main started");
   try {
-    const result = await doSomeAsyncTask((step, total) => {
+    const result = await myAsyncTask((step, total) => {
       console.log(`main progress: ${step} of ${total}`);
     });
     console.log(`main done: ${result}`);
   } catch (e) {
     console.error(e.stack);
-    // since cancellation runs finalizers for losing promise chain
-    // we don't need to force node to exit.
+    // let node exit naturally
     process.exitCode = 1;
   } finally {
+    // do cleanup, like remove tmp files
     console.log("main finalized");
   }
 }
 
 /**
- * cancellable async work task
  * @param {(i: number, t: number) => void} progress
- * @param {Race} raceCancellation
  */
-async function doSomeAsyncTask(progress) {
+async function myAsyncTask(progress) {
   try {
     for (let i = 0; i < 8; i++) {
       await doRealAsyncStep();
       progress(i + 1, 8);
     }
   } catch (e) {
-    throw new Error(`doSomeAsyncThing failed: ${e.message}`);
+    // cancellation errors can be caught to add more context
+    // to the error about the task that was cancelled
+    throw new Error(`myAsyncTask failed: ${e.message}`);
   }
   return "some result";
 }
 
-/**
- * @param {Race} raceCancellation
- */
-function doRealAsyncStep() {
-  // `disposablePromise` is helper real async to
+async function doRealAsyncStep() {
+  // `disposablePromise` is helper for real async to
   // ensure cleanup on cancellation
-  return disposablePromise(resolve => {
+  await disposablePromise(resolve => {
     const id = setTimeout(resolve, 1000);
     return () => {
       clearTimeout(id);
